178c9f310acd742c0d9eda446e39a6eeacc7af6b
leahchoi <soora913@gmail.com>
2021-05-02 19:27:51 -0700

    initial-commit

---
+++ src/app/e-info/e-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-e-info',
+  templateUrl: './e-info.component.html',
+  styleUrls: ['./e-info.component.css']
+})
+export class EInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
